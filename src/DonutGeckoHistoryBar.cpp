682394447925b35da11e9810dca3ec47c2847c56
NOSE, Takafumi <ahya365@gmail.com>
2010-08-14 23:51:00 +0900

    start the implementation of history bar. (wip)

--- src/DonutGeckoHistoryBar.cpp
+++ src/DonutGeckoHistoryBar.cpp
@@ -9,9 +9,132 @@
 #include "DonutPFunc.h"
 #include "option/MainOption.h"
 
+template <class T>  \
+NS_IMPL_ADDREF(CPlacesTreeViewCtrlImpl<T>)
+template <class T>  \
+NS_IMPL_RELEASE(CPlacesTreeViewCtrlImpl<T>)
+template <class T>  \
+NS_IMPL_QUERY_INTERFACE1(CPlacesTreeViewCtrlImpl<T>, nsINavHistoryResultViewer)
+
+/* void nodeInserted (in nsINavHistoryContainerResultNode aParent, in nsINavHistoryResultNode aNode, in unsigned long aNewIndex); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeInserted(nsINavHistoryContainerResultNode *aParent, nsINavHistoryResultNode *aNode, PRUint32 aNewIndex)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeRemoved (in nsINavHistoryContainerResultNode aParent, in nsINavHistoryResultNode aItem, in unsigned long aOldIndex); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeRemoved(nsINavHistoryContainerResultNode *aParent, nsINavHistoryResultNode *aItem, PRUint32 aOldIndex)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeMoved (in nsINavHistoryResultNode aNode, in nsINavHistoryContainerResultNode aOldParent, in unsigned long aOldIndex, in nsINavHistoryContainerResultNode aNewParent, in unsigned long aNewIndex); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeMoved(nsINavHistoryResultNode *aNode, nsINavHistoryContainerResultNode *aOldParent, PRUint32 aOldIndex, nsINavHistoryContainerResultNode *aNewParent, PRUint32 aNewIndex)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeTitleChanged (in nsINavHistoryResultNode aNode, in AUTF8String aNewTitle); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeTitleChanged(nsINavHistoryResultNode *aNode, const nsACString & aNewTitle)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeURIChanged (in nsINavHistoryResultNode aNode, in AUTF8String aNewURI); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeURIChanged(nsINavHistoryResultNode *aNode, const nsACString & aNewURI)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeIconChanged (in nsINavHistoryResultNode aNode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeIconChanged(nsINavHistoryResultNode *aNode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeHistoryDetailsChanged (in nsINavHistoryResultNode aNode, in PRTime aNewVisitDate, in unsigned long aNewAccessCount); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeHistoryDetailsChanged(nsINavHistoryResultNode *aNode, PRTime aNewVisitDate, PRUint32 aNewAccessCount)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeTagsChanged (in nsINavHistoryResultNode aNode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeTagsChanged(nsINavHistoryResultNode *aNode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeKeywordChanged (in nsINavHistoryResultNode aNode, in AUTF8String aNewKeyword); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeKeywordChanged(nsINavHistoryResultNode *aNode, const nsACString & aNewKeyword)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeAnnotationChanged (in nsINavHistoryResultNode aNode, in AUTF8String aAnnoName); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeAnnotationChanged(nsINavHistoryResultNode *aNode, const nsACString & aAnnoName)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeDateAddedChanged (in nsINavHistoryResultNode aNode, in PRTime aNewValue); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeDateAddedChanged(nsINavHistoryResultNode *aNode, PRTime aNewValue)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeLastModifiedChanged (in nsINavHistoryResultNode aNode, in PRTime aNewValue); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeLastModifiedChanged(nsINavHistoryResultNode *aNode, PRTime aNewValue)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void nodeReplaced (in nsINavHistoryContainerResultNode aParentNode, in nsINavHistoryResultNode aOldNode, in nsINavHistoryResultNode aNewNode, in unsigned long aIndex); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::NodeReplaced(nsINavHistoryContainerResultNode *aParentNode, nsINavHistoryResultNode *aOldNode, nsINavHistoryResultNode *aNewNode, PRUint32 aIndex)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void containerOpened (in nsINavHistoryContainerResultNode aContainerNode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::ContainerOpened(nsINavHistoryContainerResultNode *aContainerNode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void containerClosed (in nsINavHistoryContainerResultNode aContainerNode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::ContainerClosed(nsINavHistoryContainerResultNode *aContainerNode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void invalidateContainer (in nsINavHistoryContainerResultNode aContainerNode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::InvalidateContainer(nsINavHistoryContainerResultNode *aContainerNode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* void sortingChanged (in unsigned short sortingMode); */
+PLACESTREEVIEW(NS_IMETHODIMP) ::SortingChanged(PRUint16 sortingMode)
+{
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+/* attribute nsINavHistoryResult result; */
+PLACESTREEVIEW(NS_IMETHODIMP) ::GetResult(nsINavHistoryResult * *aResult)
+{
+	*aResult = m_result;
+    return NS_OK;
+}
+PLACESTREEVIEW(NS_IMETHODIMP) ::SetResult(nsINavHistoryResult * aResult)
+{
+	m_result = aResult;
+    return NS_OK;
+}
+
+
 // Ctor
 CDonutGeckoHistoryBar::CDonutGeckoHistoryBar()
 {
+
 }
 
 // Methods
@@ -24,6 +147,11 @@ BYTE CDonutGeckoHistoryBar::PreTranslateMessage(MSG *pMsg)
 
 LRESULT CDonutGeckoHistoryBar::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL & /*bHandled*/)
 {
+
+	m_treeview.SubclassWindow(GetDlgItem(IDC_PLACES_TREEVIEW));
+	m_treeview.ModifyStyle(0, TVS_SINGLEEXPAND | TVS_TRACKSELECT | TVS_FULLROWSELECT);
+	m_treeview.DoSearch(_T(""));
+
 	DlgResize_Init(false, true, WS_CLIPCHILDREN);
 
 	_GetProfile();
--- src/DonutGeckoHistoryBar.h
+++ src/DonutGeckoHistoryBar.h
@@ -4,12 +4,105 @@
 #include "Mtl.h"
 #include "MtlCtrls.h"
 #include "Donut.h"
+#include <nsINavHistoryService.h>
 
 template <class T>
-class CPlacesTreeViewCtrlImpl : public CWindowImpl<T, CTreeViewCtrl> {
+class CPlacesTreeViewCtrlImpl : 
+	public CWindowImpl<T, CTreeViewCtrl>, 
+	public nsINavHistoryResultViewer 
+{
 public:
+	enum SortingType {
+		FIRST_VISIT, LAST_VISIT, DATE_AND_SITE, SITE, DATE
+	};
 	DECLARE_WND_SUPERCLASS( NULL, CTreeViewCtrl::GetWndClassName() )
+
+	CPlacesTreeViewCtrlImpl()
+		:m_sortingType(DATE)
+	{
+		m_historySvc = do_GetService("@mozilla.org/browser/nav-history-service;1");
+		NS_ADDREF(this);
+	}
+	~CPlacesTreeViewCtrlImpl()
+	{
+		m_result->SetViewer(NULL);
+	}
+public:
+	// Message map and handlers
+	BEGIN_MSG_MAP(CPlacesTreeViewCtrlImpl)
+		MESSAGE_HANDLER (	WM_CREATE		, OnCreate			)
+	END_MSG_MAP()
+
+	NS_DECL_ISUPPORTS
+	NS_DECL_NSINAVHISTORYRESULTVIEWER
+
+
+	LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
+	{
+		DoSearch(_T(""));
+		bHandled = FALSE;
+		return 1;
+	}
+
+	void MakeChild(HTREEITEM parent, nsINavHistoryContainerResultNode * parentNode){
+		PRUint32 cnt;
+		parentNode->GetChildCount(&cnt);
+		for(unsigned int i = 0;i<cnt;i++){
+			nsCOMPtr<nsINavHistoryResultNode> child;
+			parentNode->GetChild(i, getter_AddRefs(child));
+			nsEmbedCString title;
+			child->GetTitle(title);
+			HTREEITEM item = 
+				InsertItem(NS_ConvertUTF8toUTF16(title).get(), parent, TVI_LAST);
+			nsCOMPtr<nsINavHistoryContainerResultNode> container = do_QueryInterface(child);
+			if(container){ 
+				container->SetContainerOpen(PR_TRUE);
+				MakeChild(item, container);
+			}
+		}
+	}
+
+	void DoSearch(const CString& str){
+		nsCOMPtr<nsINavHistoryQuery> query;
+		nsCOMPtr<nsINavHistoryQueryOptions> options;
+		m_historySvc->GetNewQuery(getter_AddRefs(query));
+		m_historySvc->GetNewQueryOptions(getter_AddRefs(options));
+		options->SetResultType(nsINavHistoryQueryOptions::RESULTS_AS_DATE_SITE_QUERY);//RESULTS_AS_DATE_QUERY);
+		m_historySvc->ExecuteQuery(query, options, getter_AddRefs(m_result));
+		m_result->SetViewer(this);
+		nsCOMPtr<nsINavHistoryContainerResultNode> root;
+		m_result->GetRoot(getter_AddRefs(root));
+		root->SetContainerOpen(PR_TRUE);
+		MakeChild(TVI_ROOT, root);
+	}
+
+	void SetSortingType(int type){
+		m_sortingType = type;
+	}
+
+	int GetSortingType(){
+		return m_sortingType;
+	}
+private:
+	nsCOMPtr<nsINavHistoryService> m_historySvc;
+	nsCOMPtr<nsINavHistoryResult> m_result;
+	int m_sortingType;
+	//CString m_keyword;
 };
+
+class CPlacesTreeViewCtrl : public CPlacesTreeViewCtrlImpl<CPlacesTreeViewCtrl> {
+public:
+	DECLARE_WND_SUPERCLASS( _T("PlacesTreeViewCtrl"), GetWndClassName() )
+};
+
+#define PLACESTREEVIEW_CTOR()	 \
+	template <class T>  \
+	CPlacesTreeViewCtrlImpl<T>
+
+#define PLACESTREEVIEW(rettype) \
+	template <class T>  \
+	rettype CPlacesTreeViewCtrlImpl<T>
+
 
 class CDonutGeckoHistoryBar
 	: public CDialogImpl<CDonutGeckoHistoryBar>
@@ -47,6 +140,7 @@ public:
 public:
 	// Dlg resize map
 	BEGIN_DLGRESIZE_MAP( CDonutGeckoHistoryBar )
+		DLGRESIZE_CONTROL( IDC_PLACES_TREEVIEW,	DLSZ_SIZE_X | DLSZ_SIZE_Y )
 	END_DLGRESIZE_MAP()
 
 	// DDX map
@@ -56,6 +150,8 @@ public:
 private:
 	LRESULT 	OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL & /*bHandled*/);
 	LRESULT 	OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL & /*bHandled*/);
+
+	CPlacesTreeViewCtrl m_treeview;
 
 	// Implementation
 	void	_GetData();
